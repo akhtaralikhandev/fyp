8d906d459365f1283f08fd7654371003f0cfb004
hammadkhanwazir <insafstudentpti@gmail.com>
2023-04-01 10:17:18 +0500

    updated

--- next.config.js
+++ next.config.js
@@ -1,23 +1,11 @@
 // next.config.js
 module.exports = {
-  async headers() {
+  async rewrites() {
     return [
       {
-        // matching all API routes
         source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-        ],
+        destination:
+          "https://6427bd07e28005414b4c0751--resonant-dolphin-61e8b3.netlify.app/:path*",
       },
     ];
   },
