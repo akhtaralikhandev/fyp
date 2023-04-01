da0ad38d7208d6fee3f75fc722f6d23320e6cbd5
hammadkhanwazir <insafstudentpti@gmail.com>
2023-04-01 10:30:47 +0500

    updated

--- pages/api/student/register.js
+++ pages/api/student/register.js
@@ -1,9 +1,15 @@
 import { mailOption, transporter } from "../../../config/nodemailer";
 import { PrismaClient } from "@prisma/client";
 import jwt from "jsonwebtoken";
+import cors from "cors"; // import cors middleware
 const prisma = new PrismaClient();
 
+const corsMiddleware = cors(); // create a new cors middleware instance
+
 const registrationHandler = async (req, res) => {
+  // use cors middleware
+  await corsMiddleware(req, res);
+
   if (req.method === "POST") {
     try {
       const { name, department_name, password, email, contact_no, reg_no } =
@@ -57,55 +63,3 @@ const registrationHandler = async (req, res) => {
 };
 
 export default registrationHandler;
-
-// import { PrismaClient, Prisma } from "@prisma/client";
-// const prisma = new PrismaClient();
-// const handler = async (req, res) => {
-//   if (req.method === "POST") {
-//     try {
-//       const { name, department_name, password, email, contact_no, reg_no } =
-//         req.body;
-//       const student = await prisma.student.create({
-//         data: {
-//           name: name,
-//           email: email,
-//           password: password,
-//           department_name: department_name,
-//           reg_no: parseInt(reg_no),
-//           contact_no: contact_no,
-//         },
-//       });
-//       console.log(student);
-//       res.status(200).json(student);
-//     } catch (error) {
-//       console.log(error);
-//       res.status(500).json(error.message);
-//     }
-//   } else if (req.method === "PUT") {
-//     try {
-//       const { email, data } = req.body;
-//       const student = await prisma.student.update({
-//         where: { email: email },
-//         data: { ...data },
-//       });
-//       if (!student) return res.status(404).json("No student found");
-//       res.status(200).json(student);
-//     } catch (error) {
-//       res.status(500).json(error);
-//     }
-//   } else if (req.method === "DELETE") {
-//     try {
-//       const { email } = req.body;
-//       const student = await prisma.student.delete({
-//         where: {
-//           email: email,
-//         },
-//       });
-//       res.status(200).json("Student deleted");
-//     } catch (error) {
-//       res.status(500).json(error);
-//     }
-//   }
-// };
-
-// export default handler;
